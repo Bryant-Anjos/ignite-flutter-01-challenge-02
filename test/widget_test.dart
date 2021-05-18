2a8165926cf2a424368ac0fbf2d9227187023259
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2021-05-18 20:15:02 -0300

    Trying to do the tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,22 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:notes/main.dart';
+import 'package:notes/create_note_page.dart';
+import 'package:notes/home_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('HomePage renders correctly', (WidgetTester tester) async {
+    final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
+      "/home": (BuildContext context) => HomePage(),
+    };
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(MaterialApp(
+      initialRoute: "/home",
+      routes: routes,
+    ));
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text("NOTES"), findsOneWidget);
+    expect(find.byWidget(Card()), findsNothing);
   });
 }
